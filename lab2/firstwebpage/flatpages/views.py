dad50a520c8a8c6be0e0d53b79407dc306f6d73b
Alina <ere-alina@yandex.ru>
2023-05-25 05:06:32 +0300

    lab2v2

--- lab2/firstwebpage/flatpages/views.py
+++ lab2/firstwebpage/flatpages/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+
+# Create your views here.    
 def home(request):
-    return HttpResponse(u'Hello, World!')
-    
+    return render(request, 'static_handler.html')
+
+#def home(request):
     #return HttpResponse(u'Hello, World!', content_type="text/plain")
