ca1162358ff5f2fe1659e4b84311a8a15fe7273d
Alina <ere-alina@yandex.ru>
2023-05-26 03:38:26 +0300

    lab3v3

--- lab3/blog/articles/models.py
+++ lab3/blog/articles/models.py
@@ -6,7 +6,7 @@ class Article(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
     created_date = models.DateField(auto_now_add=True)
-
+    
     def __unicode__ (self):
         return "%s: %s" % (self.author.username, self.title)
     def get_excerpt(self):
--- lab3/blog/articles/views.py
+++ lab3/blog/articles/views.py
@@ -1,19 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django import template
-
-
-def home(request):
-    return HttpResponse(u'Hello, World!', content_type="text/plain")
+from articles.models import Article
+from django.shortcuts import render
+from django.http import Http404
+def get_article(request, article_id):
+    try:
+        post = Article.objects.get(id=article_id)
+        return render(request, 'article.html', {"post": post})
+    except Article.DoesNotExist:
+        raise Http404
 
 def archive(request):
-    return render(request, 'archive.html')
-
-
-
-#from models import Article
-#from django.shortcuts import render
-
-#def archive(request):
-    #return render(request, 'archive.html', {"posts":
-        #Article.objects.all()})
+    return render(request, 'archive.html', {"posts": Article.objects.all()})
