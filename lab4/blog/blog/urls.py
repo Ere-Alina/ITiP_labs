d36fdf9e49d51815a7a110d893f937a2d3fb6128
Alina <ere-alina@yandex.ru>
2023-05-26 04:41:18 +0300

    lab4v1

--- lab4/blog/blog/urls.py
+++ lab4/blog/blog/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from articles import views
-from django.urls import path
+from django.urls import path, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.archive, name="archive"),]
+    path('', views.archive, name="archive"),
+    re_path(r'^article/(?P<article_id>\d+)$', views.get_article, name='get_article'),]
